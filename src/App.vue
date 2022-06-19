02bfb020dad666fae07f90f2076e74e9c8988fad
omaralabdullah051 <omaralabdullah051@gmail.com>
2022-06-19 09:14:52 +0400

    Binding Classes

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,17 @@
   <div v-html="channel"></div> 
   <h2 v-bind:id="headingId">Heading</h2>
   <button v-bind:disabled="isDisabled">Bind</button>
+  <h2 class="underline">Underline text</h2>
+  <h2 class="underline" v-bind:class="status">Status</h2>
+  <h2 v-bind:class="isPromoted && 'promoted'">Promoted Movie</h2>
+  <h2 v-bind:class="isSoldout ? 'sold-out' : 'new'">Soldout? movie</h2>
+  <h2 v-bind:class="['new', 'promoted']">Newly promoted movie</h2>
+  <h2 v-bind:class="[isPromoted && 'promoted', isSoldout ? 'sold-out' : 'new']">Array Conditional movie</h2>
+  <h2 v-bind:class="{
+    promoted: isPromoted,
+    new: !isSoldout,
+    'sold-out': isSoldout
+  }">Object conditional movie</h2>
 </template>
 
 <script>
@@ -14,7 +25,10 @@ export default {
       name:"Batman",
       channel: "<b>envolve</b>",
       headingId: 'heading',
-      isDisabled: true
+      isDisabled: true,
+      status: "success",
+      isPromoted: true,
+      isSoldout: true
       } 
   }
 }
@@ -28,5 +42,21 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.underline{
+  text-decoration: underline;
+}
+
+.promoted{
+  font-style: italic;
+}
+
+.new{
+  color: green;
+}
+
+.sold-out{
+  color: red;
 }
 </style>
