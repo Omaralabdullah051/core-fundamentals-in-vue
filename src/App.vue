65a2a874b26e302dbc6bedb62414e8154619b11c
omaralabdullah051 <omaralabdullah051@gmail.com>
2022-06-19 10:16:18 +0400

    List Rendering

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
-   <h2 v-if="num === 0">The number is zero</h2>
-   <h2 v-else-if="num < 0">The number is negative</h2>
-   <h2 v-else-if="num > 0">The number is positive</h2>
-   <h2 v-else>Not a number</h2>
+   <h2 v-for="(name,index) in names" :key="name">{{index + 1}} {{name}}</h2>
+   <h2 v-for="name in fullNames" :key="name.first">{{name.first}} {{name.last}}</h2>
 
-  <template v-if="display">
-    <h2>Batman</h2>
-    <h2>Evolution</h2>
-    <h2>Vue</h2>
-  </template>
+   <div v-for="actor in actors" :key="actor.name">
+     <h2>{{actor.name}}</h2>
+     <h3 v-for="movie in actor.movies" :key="movie">{{movie}}</h3>
+   </div>
 
-  <h2 v-show="showElement">Using v-show</h2>
-  <h2 v-if="showElement">Using v-if</h2>
+   <h2 v-for="(value,key,index) in myInfo" :key="value">{{index}} {{key}} {{value}}</h2>
 
-  <!-- The difference between  v-show and v-if directive is v-show will mount in the DOM and render in the UI even the condition is false. But it toggles the css property display:none to display:block with the specific condition. On the other hand, v-if directive will not mount in the DOM and will not render when the conditionis false. When we need to toggle the display between none to display, v-show is more applicable. Becasue Mount in the DOM again is costly-->
+   <template v-for="name in names" :key="name">
+       <h2>{{name}}</h2>
+       <hr>
+   </template>
 </template>
 
 <script>
@@ -21,9 +20,27 @@ export default {
   name: 'App',
   data(){
     return{
-      num: 5,
-      display: true,
-      showElement: false
+       names: ['Bruce', 'Clark', 'Michell'],
+       fullNames: [
+        {first: 'Bruce', last:'Wayne'},
+        {first: 'Clark', last:'Roy'},
+        {first: 'Michell', last:'Dom'},
+       ],
+       actors: [
+        {
+         name: 'Christian Bale',
+         movies: ['Batman','The prestige']
+        },
+        {
+         name: 'Di Caprio',
+         movies: ['Titanic','Inception']
+        },
+       ],
+       myInfo: {
+        name: "batman",
+        channel: "Evaluation",
+        course: "vue3"
+       }
     }
   }
 }
