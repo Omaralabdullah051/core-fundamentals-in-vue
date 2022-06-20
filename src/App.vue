248ebad8c1701fe3498812d4bca0c2b7b749b99d
omaralabdullah051 <omaralabdullah051@gmail.com>
2022-06-20 08:24:00 +0400

    added methods

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
-   <template v-for="name in names" :key="name">
-    <h2 v-if="name === 'Bruce'">{{name}}</h2>
-   </template>
+  <h2>{{2 + 3 + 5}}</h2>   
+  <h2>{{5 + 10 + 15}}</h2>   
+  <h2>Add method - {{add(2, 3, 5)}}</h2>
+  <h2>Add method - {{add(5, 10, 15)}}</h2>
+  <h2>Multiply method - {{multiply(2)}}</h2>
+  <h2>Multiply method - {{multiply(baseValue)}}</h2>
 </template>
 
 <script>
@@ -9,7 +12,16 @@ export default {
   name: 'App',
   data(){
     return{
-       names: ['Bruce', 'Clark', 'Michell'],
+     baseMultiplier: 5,
+     baseValue: 2
+    }
+  },
+  methods: {
+    add(a, b, c){
+      return a + b + c;
+    },
+    multiply(num){
+      return num * this.baseMultiplier;
     }
   }
 }
