346971c15b7f033dc38d77d299736a6ce8ec49a4
omaralabdullah051 <omaralabdullah051@gmail.com>
2022-06-20 11:35:19 +0400

    add watchers

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,11 @@
 <template>
-  <h2>Fullname- {{firstName}} {{lastName}}</h2>
-  <h2>Computed fullname- {{fullName}}</h2>
-  <button @click="changeFullName">Change fullname</button>
+  <h2>Volume Tracker (0-20)</h2>
+  <h3>Current Volume- {{volume}}</h3>
 
-  <h2>Total - {{items.reduce((total, curr) => (total = total + curr.price),0)}}</h2>
-  <h2>Computed Total - {{total}}</h2>
-  <h2>Method Total - {{getTotal()}}</h2>
-  <input type="text" v-model="country">
-
-  <template v-for="item in items" :key="item.id">
-     <h2 v-if="item.price > 100">{{item.title}} {{item.price}}</h2>
-  </template>
-
-  <h2 v-for="item in expensiveItems" :key="item.id">
-    {{item.title}} {{item.price}}
-  </h2>
+  <div>
+    <button @click="volume+=2">Increase</button>
+    <button @click="volume-=2">decrease</button>
+  </div>
 </template>
 
 <script>
@@ -22,58 +13,22 @@ export default {
   name: 'App',
   data(){
     return{
-      firstName: 'Bruce',
-      lastName: 'Wayne',
-      items: [
-        {
-          id: 1,
-          title: 'TV',
-          price: 100
-        },
-        {
-          id: 2,
-          title: 'Phone',
-          price: 200
-        },
-        {
-          id: 3,
-          title: 'Laptop',
-          price: 300
-        },
-      ],
-      country: "BD"
+      volume: 0
     }
   },
   methods: {
-    getTotal(){
-        console.log('getTotal Method');
-          return this.items.reduce((total, curr) => (total = total + curr.price),0);      
-    },
-    changeFullName(){
-      this.fullName = 'Clark kent'
-    }
+   
   },
   computed: {
-    fullName: {
-      get(){
-        return `${this.firstName} ${this.lastName}`
-      },
-      set(value){
-         const names = value.split(' ');
-         this.firstName = names[0];
-         this.lastName = names[1];
+    
+  },
+  watch: {
+    volume(newValue, oldValue){
+      if(newValue > oldValue && newValue === 16){
+        alert("Listening to a high volume for a long time may damage your hearing");
       }
-    },
-    total(){
-      console.log('Total computed');
-      return this.items.reduce((total, curr) => (total = total + curr.price),0);
-    },
-    expensiveItems(){
-      return this.items.filter(item => item.price > 100);
     }
   }
-  // Computed properties VS methods
-  // Computed properties are highly performant as they are cached calculations which only update when their dependencies change. On the other hand, methods are always recalculated when the corrosping HTML is rendered.
 }
 </script>
 
