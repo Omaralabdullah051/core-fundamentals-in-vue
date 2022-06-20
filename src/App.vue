e4768325f359b801e7e45729a5715acba260d3cb
omaralabdullah051 <omaralabdullah051@gmail.com>
2022-06-20 10:06:54 +0400

    add computed properties

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
-<!-- v-once is used to optimize the update performance. Renders the corresponding HTML element only once. That is why we don't see the updated result -->
-  <h2 v-once>{{name}}</h2> 
-  <button @click="name = 'Superman'">Change name</button>
-  <h2 v-pre>{{name}}</h2>
+  <h2>Fullname- {{firstName}} {{lastName}}</h2>
+  <h2>Computed fullname- {{fullName}}</h2>
+  <h2>Total - {{items.reduce((total, curr) => (total = total + curr.price),0)}}</h2>
+  <h2>Computed Total - {{total}}</h2>
 </template>
 
 <script>
@@ -10,11 +10,37 @@ export default {
   name: 'App',
   data(){
     return{
-      name: 'Batman'
+      firstName: 'Bruce',
+      lastName: 'Wayne',
+      items: [
+        {
+          id: 1,
+          title: 'TV',
+          price: 100
+        },
+        {
+          id: 2,
+          title: 'Phone',
+          price: 200
+        },
+        {
+          id: 3,
+          title: 'Laptop',
+          price: 300
+        },
+      ]
     }
   },
   methods: {
     
+  },
+  computed: {
+    fullName(){
+      return `${this.firstName} ${this.lastName}`
+    },
+    total(){
+      return this.items.reduce((total, curr) => (total = total + curr.price),0);
+    }
   }
 }
 </script>
@@ -24,33 +50,9 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  /* text-align: center; */
+  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
 
-label {
-  font-weight: bold;
-  display: flex;
-  margin-bottom: 5px;
-}
-input + label {
-  font-weight: bold;
-  display: inline-flex;
-  margin-right: 20px;
-}
-input[type='text'],
-textarea,
-select {
-  display: block;
-  width: 400px;
-  padding: 6px 12px;
-  font-size: 14px;
-  line-height: 1.42857143;
-  color: #555;
-  background-color: #fff;
-  background-image: none;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-}
 </style>
